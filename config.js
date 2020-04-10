4bc339419d9f66a782f7f53a13799a868040a7e4
Inzamamul Haque <mihaque313@gmail.com>
2020-04-10 23:07:26 +0530

    changed bio

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
   title: 'Inzamamul Haque',
-  subtitle: 'Here to pen my experiments with code',
+  subtitle: 'Here to pen my experience with code, life and whatever insightful I stumble upon!',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -22,7 +22,7 @@ module.exports = {
   author: {
     name: 'Inzamamul Haque',
     photo: '/photo.png',
-    bio: 'Here to pen my experiments with code',
+    bio: 'Here to pen my experience with code, life and whatever insightful I stumble upon!',
     contacts: {
       email: 'mihaque313@gmail.com',
       linkedin: 'https://www.linkedin.com/in/inzamam313/',
