fda84b8b587b0e6825a2820348c47110b980b16f
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2020-04-16 21:37:24 +0530

    removed instagram contact

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -16,7 +16,6 @@ const useSiteMetadata = () => {
                 github
                 linkedin
                 twitter
-                instagram
                 rss
               }
             }
