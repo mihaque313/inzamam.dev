9c7dbe55da4c8b3d6261a80c1a4560c0bee5d866
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2020-05-10 15:23:34 +0530

    added syntax higlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
+require("prismjs/themes/prism-tomorrow.css");
+require("prismjs/plugins/command-line/prism-command-line.css");
 
 exports.onClientEntry = () => {};
--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,43 @@ module.exports = {
     author: siteConfig.author
   },
   plugins: [
+	{
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+			inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+			noInlineHighlight: false,
+            languageExtensions: [
+              {
+                language: "superscript",
+                extend: "javascript",
+                definition: {
+                  superscript_types: /(SuperType)/,
+                },
+                insertBefore: {
+                  function: {
+                    superscript_keywords: /(superif|superelse)/,
+                  },
+                },
+              },
+            ],
+            prompt: {
+              user: "root",
+              host: "localhost",
+              global: false,
+            },
+            escapeEntities: {},
+          },
+        },
+      ],
+    },
+},
     {
       resolve: 'gatsby-source-filesystem',
       options: {
